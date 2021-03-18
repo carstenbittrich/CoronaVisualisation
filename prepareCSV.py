a87ee59011c929d304930ab96dbecafb0ff71515
Carsten Bittrich <carsten.bittrich@logmein.com>
2021-03-18 23:24:46 +0100

    adding script to pre-filter provided csv

---
+++ prepareCSV.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+"""
+"""
+
+# import os
+import sys
+import logging
+import argparse
+import pprint
+import csv
+
+csv_file = open("example.csv", "r")
+csv_content = csv.reader(csv_file)
+
+i = 0
+headers_to_keep = ['DatenstandTag', 'Datum', 'IdLandkreis', 'Landkreis', 'AnzahlFall', 'AnzahlFallNeu', 'InzidenzFallNeu', 'Kontaktrisiko']
+
+indices_to_keep = []
+
+writer = csv.writer(open("filtered.csv", "w"))
+for entry in csv_content:
+    i += 1
+
+    # parse headers (may be useful in the future)
+    if i == 1:
+        for index, column_header in enumerate(entry):
+            if column_header in headers_to_keep:
+                indices_to_keep.append(index)
+        if len(headers_to_keep) != len(indices_to_keep):
+            raise RuntimeError("Could not find all desired headers. Aborting")
+
+    items_to_keep = []
+    for column in indices_to_keep:
+        index = column - 1
+        items_to_keep.append(entry[index])
+
+    # for the moment only for whole of germany
+    if entry[3] not in ['Landkreis', 'Deutschland']:
+        break
+
+    # dump to file
+    writer.writerow(items_to_keep)
+
