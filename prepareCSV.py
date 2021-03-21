23b0f6608e1d58e353f3bf3f0066ffafec31aeb8
Carsten Bittrich <carsten.bittrich@logmein.com>
2021-03-21 16:46:27 +0100

    feat: input sanitization

--- prepareCSV.py
+++ prepareCSV.py
@@ -14,8 +14,19 @@ csv_file = open("all-series.csv", "r")
 csv_content = csv.reader(csv_file)
 
 i = 0
-headers_to_keep = ['DatenstandTag', 'Datum', 'IdLandkreis', 'Landkreis', 'AnzahlFall',
-                   'AnzahlFallNeu', 'AnzahlTodesfallNeu', 'AnzahlGenesenNeu', 'InzidenzFallNeu_7TageSumme', 'Kontaktrisiko', 'InzidenzFallNeu_7TageSumme_R']
+headers_to_keep = {
+    'DatenstandTag': {'type': int, 'default': None,  'index': None},
+    'Datum': {'type': str, 'default': None,  'index': None},
+    'IdLandkreis': {'type': int, 'default': None,  'index': None},
+    'Landkreis': {'type': str, 'default': None,  'index': None},
+    'AnzahlFall': {'type': int, 'default': 0,  'index': None},
+    'AnzahlFallNeu': {'type': int, 'default': 0,  'index': None},
+    'AnzahlTodesfallNeu': {'type': int, 'default': 0,  'index': None},
+    'AnzahlGenesenNeu': {'type': int, 'default': 0,  'index': None},
+    'InzidenzFallNeu_7TageSumme': {'type': float, 'default': 0.0,  'index': None},
+    'Kontaktrisiko': {'type': float, 'default': 100.000,  'index': None},
+    'InzidenzFallNeu_7TageSumme_R': {'type': float, 'default': 0.0,  'index': None}
+}
 
 indices_to_keep = []
 
@@ -23,29 +34,41 @@ writer = csv.writer(open("filtered.csv", "w"))
 for entry in csv_content:
     i += 1
 
-    # parse headers (may be useful in the future)
+    # parse headers
     if i == 1:
+        header_row = []
         for index, column_header in enumerate(entry):
             if column_header in headers_to_keep:
-                indices_to_keep.append(index)
-        if len(headers_to_keep) != len(indices_to_keep):
+                headers_to_keep[column_header]['index'] = index
+                header_row.append(column_header)
+
+        # check if all required columns are found
+        if not len(header_row) == len(headers_to_keep):
             raise RuntimeError("Could not find all desired headers. Aborting")
+        writer.writerow(header_row)
+        continue
 
+    # for the moment only for bundeslaender
+    if entry[4] not in ['BL']:
+        continue
+
+    # testing
+    if entry[2] != 1:
+        continue
+
+    # check compatibility
     items_to_keep = []
-    for index in indices_to_keep:
-        items_to_keep.append(entry[index])
+    for name, column in headers_to_keep.items():
+        index = column['index']
+        val = entry[index]
+        # check type
+        try:
+            parsed = column['type'](val)
+        except:
+            val = column['default']
 
-    # for the moment only for whole of germany
-    if entry[4] not in ['LandkreisTyp', 'BL']:
-        continue
-
-    if ".1.2020" in entry[1]:
-        continue
-    if ".2.2020" in entry[1]:
-        continue
-    if ".3.2020" in entry[1]:
-        continue
+        # append
+        items_to_keep.append(val)
 
     # dump to file
     writer.writerow(items_to_keep)
-
