6235ff6ceaac8e3bb48de2c797a959bc289e8664
Carsten Bittrich <carsten.bittrich@logmein.com>
2021-03-21 16:11:04 +0100

    split in bunderslaender

--- prepareCSV.py
+++ prepareCSV.py
@@ -10,11 +10,12 @@ import argparse
 import pprint
 import csv
 
-csv_file = open("example.csv", "r")
+csv_file = open("all-series.csv", "r")
 csv_content = csv.reader(csv_file)
 
 i = 0
-headers_to_keep = ['DatenstandTag', 'Datum', 'IdLandkreis', 'Landkreis', 'AnzahlFall', 'AnzahlFallNeu', 'InzidenzFallNeu', 'Kontaktrisiko']
+headers_to_keep = ['DatenstandTag', 'Datum', 'IdLandkreis', 'Landkreis', 'AnzahlFall',
+                   'AnzahlFallNeu', 'AnzahlTodesfallNeu', 'AnzahlGenesenNeu', 'InzidenzFallNeu_7TageSumme', 'Kontaktrisiko', 'InzidenzFallNeu_7TageSumme_R']
 
 indices_to_keep = []
 
@@ -35,8 +36,15 @@ for entry in csv_content:
         items_to_keep.append(entry[index])
 
     # for the moment only for whole of germany
-    if entry[3] not in ['Landkreis', 'Deutschland']:
-        break
+    if entry[4] not in ['LandkreisTyp', 'BL']:
+        continue
+
+    if ".1.2020" in entry[1]:
+        continue
+    if ".2.2020" in entry[1]:
+        continue
+    if ".3.2020" in entry[1]:
+        continue
 
     # dump to file
     writer.writerow(items_to_keep)
--- src/script.ts
+++ src/script.ts
@@ -4,14 +4,14 @@ import * as d3 from 'd3';
 
 dc.config.defaultColors(d3.schemePaired);
 
-export const paymentHist = new dc.BarChart('#payments-chart');
 export const dailyCasesChart = new dc.BarChart('#daily-cases-chart');
 export const timeRangeChart = new dc.BarChart('#time-range-chart');
+export const laenderChart = new dc.RowChart('#laender-chart');
 
 d3.csv('filtered.csv').then((germanyData) => {
+  console.log('done parsing');
   // Since its a csv file we need to format the data a bit.
   const dateFormatSpecifier = '%d.%m.%Y';
-  // const dateFormat = d3.timeFormat(dateFormatSpecifier);
   const dateFormatParser = d3.timeParse(dateFormatSpecifier);
 
   germanyData.forEach((d) => {
@@ -26,19 +26,34 @@ d3.csv('filtered.csv').then((germanyData) => {
   // Dimension by full date
   const weekDimension = data.dimension((d) => d['week']);
   const dateDimension = data.dimension((d) => d['dd']);
+  const laenderDimension = data.dimension((d) => d['Landkreis']);
   const dailyGroup = dateDimension.group();
-  const incidenceDimension = data.dimension((d) => d["InzidenzFallNeu"]);
-  const indidenceByDayGroup = dateDimension.group().reduceSum((d) => d["InzidenzFallNeu"]);
+  const laenderGroup = laenderDimension.group();
+  const indidenceByDayGroup = dailyGroup.reduceSum((d) => d["InzidenzFallNeu"]);
+  const incidenceByLandGroup = laenderGroup.reduceSum((d) => d['InzidenzFallNeu']);
 
-  paymentHist.width(800).height(380).margins({
-    top: 10, right: 50, bottom: 30, left: 40,
-  })
-    .dimension(incidenceDimension)
-    .group(indidenceByDayGroup)
-    .elasticY(true)
-    .alwaysUseRounding(true)
-    .x(d3.scaleLinear().domain([0, 50]))
-    .renderHorizontalGridLines(true);
+  laenderChart /* dc.rowChart('#day-of-week-chart', 'chartGroup') */
+    .width(800)
+    .height(280)
+    .margins({
+      top: 20, left: 10, right: 10, bottom: 20,
+    })
+    .group(incidenceByLandGroup)
+    .dimension(laenderDimension)
+  // Assign colors to each value in the x scale domain
+    // .ordinalColors(['#3182bd', '#6baed6', '#9ecae1', '#c6dbef', '#dadaeb'])
+    .label((d) => d.key.split('.')[1])
+  // Title sets the row text
+    // .title((d) => d.value)
+    .elasticX(true)
+    .xAxis()
+    // .ticks(4);
+    // .rangeChart(timeRangeChart)
+    // .x(d3.scaleTime().domain([new Date(2020, 2, 1), new Date(2021, 3, 19)]))
+    // .round(d3.timeDay.round)
+    // .elasticY(true)
+    // .alwaysUseRounding(true)
+    // .xUnits(d3.timeDays);
 
   dailyCasesChart.width(800)
     .height(380)
